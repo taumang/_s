e4f9047f9619d4f48812f29e38790057ebe0749d
Your Name <09taumang@gmail.com>
2023-07-28 21:53:51 +0200

    first commit

--- functions.php
+++ functions.php
@@ -169,16 +169,4 @@ require get_template_directory() . '/inc/template-functions.php';
  */
 require get_template_directory() . '/inc/customizer.php';
 
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
 
-/**
- * Load WooCommerce compatibility file.
- */
-if ( class_exists( 'WooCommerce' ) ) {
-	require get_template_directory() . '/inc/woocommerce.php';
-}
