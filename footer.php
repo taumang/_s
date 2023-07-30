8a294e1701b80da7063af3cca0e3fac6f2e917c9
Your Name <09taumang@gmail.com>
2023-07-31 00:28:27 +0200

    content added in front-page

--- footer.php
+++ footer.php
@@ -17,6 +17,14 @@
 					</ul>
 				</div>
 	</footer>
+
+            <script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
 	
 <?php wp_footer(); ?>
 
