a48dfa44211f7ff75a45117ff3321fb687d1e04f
Your Name <09taumang@gmail.com>
2023-07-31 21:41:33 +0200

    must fix 2 webpack errors

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,6 +2,6 @@ import "./default/jquery.min.js";
 import "./default/jquery.scrollex.min.js";
 import "./default/jquery.scrolly.min.js";
 import "./default/browser.min.js";
-import "./default/breakpoint.min.js";
+import "./default/breakpoints.min.js";
 import "./default/util.js";
 import "./default/main.js";
